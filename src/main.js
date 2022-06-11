2c5d723f79af4d1083c4052a511e9c25d4190f9c
qzsIU <383163566@qq.com>
2022-06-12 07:59:15 +0800

    引入自定义阿里icon图标字体、引入vant移动端组件

--- src/main.js
+++ src/main.js
@@ -3,11 +3,14 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import './styles/index.less'
+import Vant from 'vant'
+import 'vant/lib/index.css'
 
+Vue.use(Vant)
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App)
 }).$mount('#app')
